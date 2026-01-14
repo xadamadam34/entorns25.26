c9e88de03bb9525bea2f802174bac06f500fd897
xadamadam34 <tangermalak10@gmail.com>
2026-01-14 13:57:30 +0100

    server.py

--- Test_Server/server.py
+++ Test_Server/server.py
@@ -1,37 +1,119 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 
+class User:
+    def __init__(self,username, nom, password, email, rol="tutor"):
+        self.username=username
+        self.nom=nom
+        self.password=password
+        self.email=email
+        self.rol=rol
+    
+    def __str__(self):
+        return self.nom
+
+#us1=User(username="ama",nom="Rob Halford",password="12345", email="rob@gmail.com",rol="tutor")
+#print(us1)
+users = [
+    User(username="rob",nom="Rob Halford",password="12345", email="rob@gmail.com",rol="tutor"),
+    User(username="john",nom="John Cannigan",password="12345", email="john@gmail.com",rol="tutor"),
+    User(username="maria",nom="Maria Sams",password="12345", email="maria@gmail.com",rol="admin")
+]
+
+class UserDao:
+    def __init__(self):
+        self.users=users
+    
+    def getUserByUsername(self,uname):
+        user = None
+        for u in self.users:
+            if u.username == uname:
+                user = u.__dict__
+        return user
+    def getApellidoByUsername(self, uname):
+        for u in self.users:
+            if u.username == uname:
+                return u.nom.split(" ")[1]  
+    
+    def getAllUsers(self):
+        # Devuelve todos los usuarios como diccionarios
+        return [u.__dict__ for u in self.users]
+    
+        return None
+    def getAllUsernames(self):
+        return [u.username for u in self.users]
+
+# Test DAO
+'''user_dao = UserDao()
+response=user_dao.getUserByUsername("maria")
+print(response)
+response=user_dao.getUserByUsername("AAAA")
+print(response) '''
+# End TEST
+
+# Instanciem el Dao User
+user_dao = UserDao()
 
 app = Flask(__name__)
 
+@app.route('/user',methods=['GET'])
+def user():
+    resposta=""
+    # Parametres
+    username = request.args.get("username",default="")
+    # Si els paràmetres OK
+    if username != "":
+        # Anar al DAO Server i cercar User per username
+        resposta=user_dao.getUserByUsername(username)
+        # respondre amb dades Ususari si trobat
+        if resposta == None:
+            resposta = {"msg":"Usuari No troba t"}
+    else:  #  Si els paràmetres NO ok 
+        # respondre error
+        resposta = {"msg":"Falta paràmetre Username"}
+    
+    return jsonify(resposta)
 
-# --- Ruta GET ---
-# Ejemplo: http://localhost:5000/saludo?nombre=Juan
-@app.route('/saludo', methods=['GET'])
-def saludo():
-    nombre = request.args.get('nombre')  # Parámetro en la URL
-    if not nombre:
-        return jsonify({'error': 'Falta el parámetro "nombre"'}), 400
-    return jsonify({'mensaje': f'Hola, {nombre}!'})
+@app.route('/usernames', methods=['GET'])
+def usernames():
+    usernames = user_dao.getAllUsernames()
+    return jsonify(usernames)
+
+@app.route('/allusernames', methods=['GET'])
+def allusernames():
+    all_users = user_dao.getAllUsers()
+    return jsonify(all_users)
 
 
 
 
-# --- Ruta POST ---
-# Ejemplo: POST a http://localhost:5000/sumar con body JSON {"numero": 5}
-@app.route('/sumar', methods=['POST'])
-def sumar():
-    data = request.get_json() or request.form  # Soporta JSON y form-data
-    numero = data.get('numero')
-    if numero is None:
-        return jsonify({'error': 'Falta el parámetro "numero"'}), 400
-    try:
-        resultado = int(numero) + 10
-    except ValueError:
-        return jsonify({'error': '"numero" debe ser un entero'}), 400
-    return jsonify({'resultado': resultado})
+@app.route('/apellido', methods=['GET'])
+def apellido():
+    username = request.args.get("username")
+
+    if not username:
+        return jsonify({"msg": "Falta parámetro username"}), 400
+
+    apellido = user_dao.getApellidoByUsername(username)
+
+    if apellido is None:
+        return jsonify({"msg": "Usuario no encontrado"}), 404
+
+    return jsonify({"apellido": apellido})
 
 
-
-
+class daoUserClient:
+    def getUsernames(self):
+        #Petició Http al WebService
+        # TO-DO
+        return None
+    
+class ViewConsole:
+    def getInputUsername():
+        # TO-DO
+        return None
+    def showUserInfo(user):
+        # TO-DO
+        return None
+    
 if __name__ == '__main__':
     app.run(debug=True)
