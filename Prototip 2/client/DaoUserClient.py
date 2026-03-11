0f96ca6b675f81292c20d97c559149d1aa4f5346
xadamadam34 <tangermalak10@gmail.com>
2026-03-11 13:41:18 +0100

    Update DaoUserClient.py

--- Prototip 2/client/DaoUserClient.py
+++ Prototip 2/client/DaoUserClient.py
@@ -1,29 +1,37 @@
 import requests
-from dadesserver import User
-from User import *
-from flask import jsonify
+from User import User
 
 class DaoUserClient:
+
     base_url = "http://localhost:5000"
 
-    def login(self,user):
-        Url_peticioN = self.base_url + "/login"
+    def login(self, user):
+
+        url_peticion = self.base_url + "/login"
+
         params_post = {
             "username": user.username,
             "password": user.password
         }
-        response = requests.post(Url_peticioN, json=params_post)
+
+        response = requests.post(url_peticion, json=params_post)
+
         if response.status_code == 200:
+
             user_data_raw = response.json()
             code_response = user_data_raw['coderesponse']
+
             if code_response == "0":
                 return None
-            else :
-                user = User(user_data_raw['id'], user_data_raw['username'], "", user_data_raw['email'], user_data_raw['idrole'], user_data_raw['token'])
-        else:
-            return None
-        
-daoClient = DaoUserClient()
-user= User("", "mare", "", "12345", "")
-resposta = daoClient.login(user)
-print(resposta)
+            else:
+                user = User(
+                    user_data_raw['id'],
+                    user_data_raw['username'],
+                    "",
+                    user_data_raw['email'],
+                    user_data_raw['idrole'],
+                    user_data_raw['token']
+                )
+                return user
+
+        return None
