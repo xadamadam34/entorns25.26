fde1b6f0b59dbb615ac378330323f6893055621c
xadamadam34 <tangermalak10@gmail.com>
2026-04-13 14:40:48 +0200

    Add files via upload

---
+++ Prototip 3/Server/DaoServer.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+from dadesserver import *
+
+app = Flask(__name__)
+
+
+class UserDAO:
+
+    
+    def login(self, identifier, password):
+        return None
+
