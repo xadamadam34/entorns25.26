78a92acfb8b8044fddd0656c76d982ea163635cc
xadamadam34 <tangermalak10@gmail.com>
2026-04-15 12:09:31 +0200

    Add files via upload

--- Prototip 3/Server/DaoServer.py
+++ Prototip 3/Server/DaoServer.py
@@ -1,12 +1,38 @@
 from flask import Flask, request, jsonify
-from dadesserver import *
+import mysql
+from dadesServerr import *
+import mysql.connector
 
 app = Flask(__name__)
 
 
 class UserDAO:
+       
 
-    
+    def connectBBDD(self):
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="tapatapp"
+        )
+        return connection       
+
     def login(self, identifier, password):
-        return None
+        # connexió a BBDD
+        con = self.connectBBDD()
+        cursor = con.cursor(dictionary=True)
+        query = """
+          SELECT * FROM User
+          WHERE (username = %s OR email = %s) AND password = %s
+          """
+        cursor.execute(query, (identifier, identifier, password))
+        user = cursor.fetchone()
+        cursor.close()
+        con.close()
+        return user
+
+dao=UserDAO()
+u=dao.login("mare", "mare")
+print(u)
 
