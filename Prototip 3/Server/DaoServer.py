450ba08b3b42c73eeed5b899877e21c9e0cf6d35
xadamadam34 <tangermalak10@gmail.com>
2026-04-22 12:53:12 +0200

    Update DaoServer.py

--- Prototip 3/Server/DaoServer.py
+++ Prototip 3/Server/DaoServer.py
@@ -1,7 +1,11 @@
+import hashlib
+import random
+import token
 from flask import Flask, request, jsonify
 import mysql
 from dadesServerr import *
 import mysql.connector
+from time import time
 
 app = Flask(__name__)
 
@@ -28,11 +32,49 @@ class UserDAO:
           """
         cursor.execute(query, (identifier, identifier, password))
         user = cursor.fetchone()
+        if user:
+           self.setTokenUser(user['username'])
+        print
+        user ['token'] = token
         cursor.close()
         con.close()
         return user
+    
+    def setTokenUser(self, username):
+        # connexió a BBDD
+        con = self.connectBBDD()
+        cursor = con.cursor(dictionary=True)
+        # Generar un token
+        token = self.getHash(username)
+        # update del token a la BBDD
+        print (type(token))
+        query = " UPDATE User SET token = '" + token + "' Where username = '" + username + "'"
+        print(query)
+        cursor.execute(query)
+        con.commit()
+        # Close connexió a BBDD
+        cursor.close()
+        con.close()
+
+
+    def getHash2(self, username):
+        miliseconds = str(time() * 1000)
+        data = username + (miliseconds)
+        hash_object = hashlib.sha256(data.encode('utf-8'))
+        return hash_object.hexdigest()
+    
+    def getHash(self, username):
+        miliseconds = str(time() * random.randrange(1000))
+        data = username + (miliseconds)
+        hash_object = hashlib.sha256(data.encode('utf-8'))
+        return hash_object.hexdigest() + ""
+
+
+        
+
 
 dao=UserDAO()
 u=dao.login("mare", "mare")
 print(u)
+
 
